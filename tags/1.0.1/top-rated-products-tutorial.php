c6b7706a03aae403c1db39cce3fcdffb38428a80
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-04-24 08:19:44 -0500

    Merge development with production from 1.0.1 to 1.0.2

---
+++ tags/1.0.1/top-rated-products-tutorial.php
@@ -0,0 +1,14 @@
+<?php
+
+function top_rated_products_tutorial()
+{
+	?>
+	<h1>Top Rated Products Plugin</h1>
+	<p>Version 1.0 | <a href="admin.php?page=top-rated-products" >Options</a> | <a href="admin.php?page=top-rated-products-faq" >FAQ</a> | <a href="admin.php?page=top-rated-products-tutorial" >Tutorial</a></p>
+	<hr/>
+	<p>Tutorial text goes here</p>
+
+	
+	<?php
+}
+?>
